dc643e6ec19b3b63e314ac5121660440ef1a6fe0
Raabdulali <67294234+Raabdulali@users.noreply.github.com>
2025-01-29 12:58:05 -0800

    config

--- buckets.h
+++ buckets.h
@@ -1,5 +1,9 @@
 #ifndef BUCKETS
 #define BUCKETS
 
+struct bucket{
+	
+}
+
 
 #endif
---
+++ config.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const int bucket_size = 4;
+const int block_size = 32;
+
+#endif
