82ac8b91725356f719d76f0cb9c062c676390419
Raabdulali <67294234+Raabdulali@users.noreply.github.com>
2025-02-13 23:22:04 -0800

    testing file created

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,6 +4,8 @@
 #include "../include/bst.h"
 #include "../include/encryption.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <cmath>
 #include <memory>
 #include <utility>
@@ -11,10 +13,9 @@
 using namespace std;
 
 int main() {
-    // initial paramameters
-    int num_blocks = 1000; //actually lower bound for buckets, should fix
+    // initial parameters
+    int num_blocks = 10000; // actually lower bound for buckets, should fix
     int bucket_capacity = 4;
-    //int L = ceil(num_blocks/4);
     int L = ceil(log2(num_blocks));
     
     // num buckets in oram
@@ -30,46 +31,29 @@ int main() {
     Server server(num_blocks, bucket_capacity, move(oram_tree));
     // init client
     Client client(num_blocks, &server, encryptionKey);
-    //server.printHeap();
-    
-    cout << "Writing blocks." << endl;
-    for (int i = 0; i < 2000; i++) {
-        string data = to_string(i);
-        client.access(1, i, data);
+
+    // Read dataset file and write blocks from it
+    string datasetPath = "/Users/rabdulali/Desktop/Path-ORAM/tests/2^20.txt";
+    ifstream infile(datasetPath);
+    string line;
+    while(getline(infile, line)) {
+        istringstream iss(line);
+        string id_str, data;
+        if(getline(iss, id_str, ',') && getline(iss, data)) {
+            int id = stoi(id_str);
+            data.erase(0, data.find_first_not_of(" \t"));
+            client.access(1, id, data);
+        }
     }
-    
+    infile.close();
 
-    //cout << "reading blocks" << endl;
-    cout << "trying to read" << endl;
-    client.access(0,48,"").print_block();
-    client.access(0,86,"").print_block();
-    client.access(0,1834,"").print_block();
-    client.access(0,1834,"").print_block();
-    client.access(0,1834,"").print_block();
-    client.access(0,1834,"").print_block();
-    client.access(0,1834,"").print_block();
-    client.access(0,1834,"").print_block();
+    // Example read accesses:
+    cout << "Trying to read blocks." << endl;
+    client.access(0, 0, "").print_block();
+    client.access(0, 999, "").print_block();
 
-    //server.printHeap();
-    //server.printHeap();
-
-    //cout << "printing oram" << endl;
-    //server.printHeap();
-    cout << "printing stash" << endl;
-    client.print_stash();
-
-    /*
-    client.access(1, 0, "first block");
-    cout << "printing oram" << endl;
-    server.printHeap();
-    cout << "printing stash" << endl;
-    client.print_stash();
-    */
-
-    //client.access(0, 3, "").print_block();
-
+    //cout << "Printing stash:" << endl;
     //client.print_stash();
-    //server.printHeap();
 
     return 0;
 }
