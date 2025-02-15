74b7f766ba0f4cc8283512f87a6d7f9a01237a16
Raabdulali <67294234+Raabdulali@users.noreply.github.com>
2025-02-14 17:10:18 -0800

    performance graph for path path_oram

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 int main() {
     // initial parameters
-    int num_buckets_low = 5; // lower bound for buckets - fixed
+    int num_buckets_low = 130000; // lower bound for buckets - fixed
     cout << "num buckets" << num_buckets_low << endl;
     int bucket_capacity = 4;
     int L = ceil(log2(num_buckets_low));
@@ -35,7 +35,7 @@ int main() {
     Client client(num_buckets_low, &server, encryptionKey);
 
     // Read dataset file and write blocks from it
-    string datasetPath = "/Users/rabdulali/Desktop/Path-ORAM/tests/2^20.txt";
+    string datasetPath = "/Users/rabdulali/Desktop/Path-ORAM/tests/2^18.txt";
     ifstream infile(datasetPath);
     string line;
     while(getline(infile, line)) {
@@ -51,8 +51,8 @@ int main() {
 
     // Example read accesses:
     cout << "Reading Blocks:" << endl;
-    for (int i = 0; i < 10; i++) {
-        client.access(0, i, "").print_block();
+    for (int i = 0; i < 262144; i++) {
+        client.access(0, i, "");
     }
 
     /*
@@ -64,9 +64,9 @@ int main() {
     */
 
     cout << "printing server view" << endl;
-    server.printHeap();
+    //server.printHeap();
     cout << "Printing stash:" << endl;
-    client.print_stash();
+    //client.print_stash();
 
     return 0;
 }
