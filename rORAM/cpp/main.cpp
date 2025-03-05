5610288521c4c140d9d4625a935d74dd1f879a2b
roarosales <r_rosales_@outlook.com>
2025-03-05 15:38:52 -0800

    fixed include on client file

--- rORAM/cpp/main.cpp
+++ rORAM/cpp/main.cpp
@@ -14,10 +14,10 @@ using namespace std;
 
 int main() {
     // Number of blocks to test
-    int num_blocks = 10000;
+    int num_blocks = 1000;
     
     // Query range parameters
-    int query_range = 10000;  // Default range size for queries
+    int query_range = 100;  // Default range size for queries
     
     // Calculate appropriate ORAM parameters
     // Buckets should be 2^i-1 where i makes it larger than num_blocks
